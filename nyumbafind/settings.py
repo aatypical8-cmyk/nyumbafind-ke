d72bf654b5b5a56ab68ef3d29216d9b1d5cbee6a
Omwanda Gerald <omwandagerald7@gmail.com>
2026-06-26 20:52:26 +0300

    Update settings.py for stable deployment

--- nyumbafind/settings.py
+++ nyumbafind/settings.py
@@ -3,26 +3,18 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = 'django-insecure-change-this-in-production-nyumbafind-ke-2026'
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.railway.app',
-    'https://*up.railway.app',
+    'https://*.up.railway.app',
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-LOGIN_REDIRECT_URL ='home'
-LOGOUT_REDIRECT_URL = 'home'
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -82,3 +74,14 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Africa/Nairobi'
 USE_I18N = True
 USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
